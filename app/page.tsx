1a22fc3bdc031e30ad121f6741e94e42cca5f2e1
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2024-01-04 21:04:17 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Intro from '@/components/intro';
+import SectionDivider from '@/components/section-divider';
+
+export default function Home() {
+  return (
+    <main className=' flex flex-col items-center px-4'>
+      <Intro />
+      <SectionDivider />
+    </main>
+  );
+}
---
+++ components/intro.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import Image from 'next/image';
+import React from 'react';
+import id_img from '@/public/id_photo.jpg';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+import { FaGithubSquare } from 'react-icons/fa';
+
+export default function Intro() {
+  return (
+    <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
+      <div className='flex items-center justify-center'>
+        <div className='relative'>
+          <motion.div
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{
+              type: 'tween',
+              duration: 0.2,
+            }}>
+            <Image
+              src={id_img}
+              alt='iakiv_photo'
+              quality='95'
+              priority={true}
+              className=' h-24 w-24 rounded-full object-cover border-[0.25rem] border-white shadow-xl'
+            />
+          </motion.div>
+
+          <motion.span
+            className='text-4xl absolute bottom-0 right-0 '
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}>
+            👋
+          </motion.span>
+        </div>
+      </div>
+      <motion.h1
+        className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}>
+        <span className='font-bold'>Hello, I'm Iakiv.</span> I'm a{' '}
+        <span className='font-bold'>front-end developer</span> with{' '}
+        <span className='font-bold'>1 year</span> of experience. I enjoy
+        building <span className='italic'>sites & apps</span>. My focus is{' '}
+        <span className='underline'>React (Next.js)</span>.
+      </motion.h1>
+
+      <motion.div
+        className='flex flex-col sm:flex-row justify-center items-center gap-2 sm:gap-4 py-8 text-lg font-medium'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}>
+        <Link
+          href='#contact'
+          className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>
+          Contact me here{' '}
+          <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
+        </Link>
+        <a
+          className='bg-white  group py-4 flex items-center px-7 justify-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10'
+          href='/CV.pdf'
+          download>
+          Download CV{' '}
+          <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
+        </a>
+        <a
+          className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer  border-black/10'
+          href='https://www.linkedin.com/in/iakiv-pekarskyi/'
+          target='_blank'>
+          <BsLinkedin />
+        </a>
+        <a
+          className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border-black/10'
+          href='https://github.com/iakivpekarskyi'
+          target='_blank'>
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
