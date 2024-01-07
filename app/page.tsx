68e587635d9952ecbf8d874f7fdf9af713ef566d
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2024-01-07 15:35:37 +0100

    feat: add header scroll sync

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import SectionDivider from '@/components/section-divider';
 
 export default function Home() {
   return (
-    <main className=' flex flex-col items-center px-4'>
+    <main className=' flex flex-col items-center px-4 '>
       <Intro />
       <SectionDivider />
       <About />
--- components/about.tsx
+++ components/about.tsx
@@ -1,17 +1,16 @@
 'use client';
 
 import React from 'react';
-// import SectionHeading from './section-heading';
 import { motion } from 'framer-motion';
-// import { useSectionInView } from '@/lib/hooks';
 import SectionHeading from './section-heading';
+import { useSectionInView } from '@/lib/hook';
 
 export default function About() {
-  //   const { ref } = useSectionInView('About');
+  const { ref } = useSectionInView('About');
 
   return (
     <motion.section
-      // ref={ref}
+      ref={ref}
       className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28'
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
@@ -22,7 +21,7 @@ export default function About() {
         After graduating with a degree in{' '}
         <span className='font-medium'>Accounting</span>, I decided to pursue my
         passion for programming. I enrolled in a coding bootcamp and learned{' '}
-        <span className='font-medium'>full-stack web development</span>.{' '}
+        <span className='font-medium'> full-stack web development</span>.{' '}
         <span className='italic'>My favorite part of programming</span> is the
         problem-solving aspect. I <span className='underline'>love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
